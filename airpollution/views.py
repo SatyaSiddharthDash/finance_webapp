c14f67ad6da61b4d768775fb3d9725ed05e98097
SatyaSiddharthDash <dash.sathyasiddharth@gmail.com>
2022-05-03 19:29:40 +0530

    modify views to handle file upload

--- airpollution/views.py
+++ airpollution/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django import forms
+
+
+class ExcelUploadForm(forms.Form):
+    year = forms.CharField(max_length=4)
+    file = forms.FileField()
 
 
 def welcome(request):
@@ -9,6 +15,13 @@ def welcome(request):
 
 
 def upload_file(request):
+    if request.method == 'POST':
+        form = ExcelUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['file']  # this is the name of the form tag in the html template
+    else:
+        pass
+
     context = {
         'app_name': request.resolver_match.app_name,
         'message_success': 'The file has been uploaded successfully!'
